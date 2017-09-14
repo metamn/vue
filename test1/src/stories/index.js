2ecb3e39f3bc77ab5840668ed85496f85def6176
metamn <bartus.csongor@gmail.com>
2017-09-14 16:53:28 +0300

    Storybook Options addon done

--- test1/src/stories/index.js
+++ test1/src/stories/index.js
@@ -9,6 +9,76 @@ import { withInfo } from '@storybook/addon-info'
 import ButtonReadme from './README.md'
 import withReadme from 'storybook-readme/with-readme'
 
+import { setOptions } from '@storybook/addon-options';
+
+setOptions({
+  /**
+   * name to display in the top left corner
+   * @type {String}
+   */
+  name: 'Storybook XX',
+  /**
+   * URL for name in top left corner to link to
+   * @type {String}
+   */
+  url: '#',
+  /**
+   * show story component as full screen
+   * @type {Boolean}
+   */
+  goFullScreen: false,
+  /**
+   * display left panel that shows a list of stories
+   * @type {Boolean}
+   */
+  showLeftPanel: true,
+  /**
+   * display horizontal panel that displays addon configurations
+   * @type {Boolean}
+   */
+  showDownPanel: true,
+  /**
+   * display floating search box to search through stories
+   * @type {Boolean}
+   */
+  showSearchBox: false,
+  /**
+   * show horizontal addons panel as a vertical panel on the right
+   * @type {Boolean}
+   */
+  downPanelInRight: true,
+  /**
+   * sorts stories
+   * @type {Boolean}
+   */
+  sortStoriesByKind: false,
+  /**
+   * regex for finding the hierarchy separator
+   * @example:
+   *   null - turn off hierarchy
+   *   /\// - split by `/`
+   *   /\./ - split by `.`
+   *   /\/|\./ - split by `/` or `.`
+   * @type {Regex}
+   */
+  hierarchySeparator: null,
+
+  /**
+   * sidebar tree animations
+   * @type {Boolean}
+   */
+  sidebarAnimations: true,
+
+  /**
+   * id to select an addon panel
+   * @type {String}
+   */
+  selectedAddonPanel: undefined, // The order of addons in the "Addons Panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
+});
+
+//storybook.configure(() => require('./'), module);
+
+
 import MyButton from './MyButton.vue'
 import Welcome from './Welcome.vue'
 
