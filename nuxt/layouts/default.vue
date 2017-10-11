51fd3160ac3a9b38807baa650c82d8a8f24c24ea
metamn <bartus.csongor@gmail.com>
2017-10-11 14:54:05 +0300

    Setting up nuxt

---
+++ nuxt/layouts/default.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <nuxt/>
+</template>
+
+<script>
+  // from https://www.npmjs.com/package/normalize.css/
+  import 'normalize.css'
+</script>
+
+<style lang="scss">
+  // from https://github.com/vuejs-templates/webpack/issues/121
+  @import "../components/framework/reset";
+</style>
