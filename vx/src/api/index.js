ef167a157870dc59d9f0edf06591822a4650f1e0
metamn <bartus.csongor@gmail.com>
2017-11-29 18:39:37 +0200

    Playing with Vuex and Contentful

---
+++ vx/src/api/index.js
@@ -0,0 +1,19 @@
+import {createClient} from 'contentful'
+
+const client = createClient({
+  space: 'lvp1p3se851j',
+  accessToken: 'a8dcf7a9aeb2b6898fc58b0dae20d92014fc6ae72c06c61276293d300e4001f7'
+})
+
+export default {
+  async syncStoreWithBackend () {
+    client.getEntries({
+      content_type: 'map'
+    })
+    .then((response) => {
+      const maps = response.items.map(item => item.fields.name)
+      this.$store.commit('setMaps', maps)
+    })
+    .catch(console.error)
+  }
+}
