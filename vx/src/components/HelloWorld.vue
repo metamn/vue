956536e02d582e3feb4dd9cb2b50bf2981e8fbe8
metamn <bartus.csongor@gmail.com>
2017-11-29 17:51:35 +0200

    Playing with Vuex

---
+++ vx/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    maps: {{ maps }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    computed: {
+      maps () {
+        return this.$store.state.maps.join(', ')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ vx/src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    maps: [
+      'Valley',
+      'Frontier',
+      'Beyond',
+      'Wilderness'
+    ]
+  }
+})
